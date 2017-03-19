c6aa2f170183c96f96872e7578acbaa014feeb3c
tmushayahama <tmushayahama@gmail.com>
2017-03-18 17:58:51 -0700

    ENH - Added the api definitions to get the cat component by location, x, y coordinates

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -24,23 +24,6 @@ Route::group(['prefix' => 'api'], function() {
  Route::get('logout', 'AuthenticateController@logout');
  Route::get('authenticate/user', 'AuthenticateController@getAuthenticatedUser');
 
- //Constants
- Route::get('constants/all', 'ConstantsController@getConstants');
- Route::get('constants/level/{parent}', 'ConstantsController@getLevel');
- Route::get('constants/componenttypes', 'ConstantsController@getComponentTypes');
-
- //Search
- Route::get('search/keyword/{keyword}', 'SearchController@keywordSearch');
- Route::get('search/suggestion/keyword/{keyword}', 'SearchController@suggestionSearch');
-
- //Profile
- Route::get('profile/{id}/connections', 'Profile\ProfileController@getUserConnections');
- Route::get('profiles/all', 'Profile\ProfileController@getProfilesAll');
- Route::get('profile/{id}/sections', 'Profile\ProfileController@getUserProfileSections');
- Route::get('profile/{id}', 'Profile\ProfileController@getProfile');
- Route::post('user/request/create', 'Community\CommunityController@createRequest');
-
-
  //Component
  Route::get('components/component/x/{x}/y/{y}', 'Component\ComponentController@getComponentByLocation');
 });
